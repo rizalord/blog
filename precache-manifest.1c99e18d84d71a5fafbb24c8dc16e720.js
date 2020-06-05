53628f309ef69aa6985b3994c73cca180bb9ecf4
rizalord <ahmadkhamdani9@gmail.com>
2020-06-05 13:58:20 +0700

    initial commit

---
+++ precache-manifest.1c99e18d84d71a5fafbb24c8dc16e720.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85e31536afed547c1388",
+    "url": "/blog/css/app.8067002e.css"
+  },
+  {
+    "revision": "fef4b2314a993a74f1ca",
+    "url": "/blog/css/chunk-vendors.ef86a09a.css"
+  },
+  {
+    "revision": "0d4629e470a6ec497e5a1b1bbeaef323",
+    "url": "/blog/img/my-photo.0d4629e4.jpg"
+  },
+  {
+    "revision": "7dff8721fc0901254181c9434e5b3f22",
+    "url": "/blog/index.html"
+  },
+  {
+    "revision": "85e31536afed547c1388",
+    "url": "/blog/js/app.987a46ad.js"
+  },
+  {
+    "revision": "fef4b2314a993a74f1ca",
+    "url": "/blog/js/chunk-vendors.7e76a79e.js"
+  },
+  {
+    "revision": "5f7cc13ee2d01b18cc419d2b8497fcb9",
+    "url": "/blog/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/blog/robots.txt"
+  }
+]);
